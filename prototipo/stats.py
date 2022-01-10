5aedfa2b9d4ee03558bbdc14375aeb6192135d88
Rafael Vargas <rafael.s.vargas18@gmail.com>
2022-01-10 14:11:15 -0400

    Adicionando arquivo com status iniciais de personagens

---
+++ prototipo/stats.py
@@ -0,0 +1,39 @@
+JogadorStats = {
+    'vida': 10,
+    'ataque': 3,
+    'defesa': 2,
+    'vel': 2,
+    'vel_ataque': 1,
+    'arma_dano': 3,
+    'arma_alcance': 3
+}
+
+Inimigo1Stats = {
+    'vida': 5,
+    'ataque': 4,
+    'defesa': 1,
+    'vel': 3,
+    'vel_ataque': 1,
+    'arma_dano': 1,
+    'arma_alcance': 1
+}
+
+Inimigo2Stats = {
+    'vida': 7,
+    'ataque': 2,
+    'defesa': 2,
+    'vel': 2,
+    'vel_ataque': 1,
+    'arma_dano': 2,
+    'arma_alcance': 1
+}
+
+Inimigo3Stats = {
+    'vida': 15,
+    'ataque': 2,
+    'defesa': 4,
+    'vel': 2,
+    'vel_ataque': 1,
+    'arma_dano': 3,
+    'arma_alcance': 2
+}
