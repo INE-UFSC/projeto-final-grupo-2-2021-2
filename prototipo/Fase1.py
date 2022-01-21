963b0d32d709655a02379bf5c969d91d4b62a1f6
Paulo Eller <pauloebehr@gmail.com>
2022-01-20 21:06:46 -0300

    Adição das classes fase 1 e Hitbox

--- prototipo/Fase1.py
+++ prototipo/Fase1.py
@@ -1,19 +1,9 @@
-import pygame
 from Jogador import Jogador
-from Opcoes import Opcoes
+from Opcoes import Opcoes, Dificuldade
 from TelaJogo import TelaJogo
 from Terreno1 import Terreno1
 from Inimigo1 import Inimigo1
 from abstractions.AbstractFase import AbstractFase
-
-QUANT_INIMIGOS = 5
-INIMIGOS_POSICOES = [
-    (600, 350),
-    (380, 50),
-    (150, 85),
-    (500, 50),
-    (40, 200)
-]
 
 
 class Fase1(AbstractFase):
@@ -21,15 +11,45 @@ class Fase1(AbstractFase):
         self.__terreno = Terreno1(inimigos=[], itens=[], tamanho_tela=tamanho_tela)
         self.__dificuldade = opcoes.dificuldade
         self.__jogador = jogador
+        self.__INIMIGOS_POSICOES = []
+
+        self.__determinar_inimigos(opcoes.dificuldade)
+
+    @property
+    def jogador(self) -> Jogador:
+        return self.__jogador
+
+    @property
+    def terreno(self) -> Terreno1:
+        return self.__terreno
 
     def load(self) -> None:
         inimigos = []
-        for x in range(QUANT_INIMIGOS):
-            inimigos.append(Inimigo1(INIMIGOS_POSICOES[x], self.__dificuldade))
+        for x in range(len(self.__INIMIGOS_POSICOES)):
+            inimigos.append(Inimigo1(self.__INIMIGOS_POSICOES[x], self.__dificuldade))
         self.__terreno.load_inimigos(inimigos)
 
     def has_ended(self) -> bool:
         return self.__terreno.has_ended()
 
     def start(self, tela: TelaJogo):
-        self.__terreno.rodar_jogo(tela)
+        self.__terreno.iniciar_rodada(tela, self.__jogador)
+
+    def __determinar_inimigos(self, dificuldade: Dificuldade) -> None:
+        if dificuldade == Dificuldade.facil:
+            self.__INIMIGOS_POSICOES = [(250, 100), (350, 50), (150, 100),
+                                        (500, 50), (250, 50)]
+
+        elif dificuldade == Dificuldade.medio:
+            self.__INIMIGOS_POSICOES = [(250, 100), (350, 50), (150, 100),
+                                        (500, 50), (250, 50), (250, 250)]
+        else:
+            self.__INIMIGOS_POSICOES = [(250, 150), (350, 50), (150, 100),
+                                        (500, 50), (250, 50), (250, 100),
+                                        (100, 50), (450, 50)]
+
+    def ciclo(self, tela) -> None:
+        """Função para ser executada em todo ciclo do main loop"""
+        for inimigo in self.__terreno.inimigos:
+            inimigo.mover(self.__jogador.hitbox)
+        self.__terreno.desenhar(tela, self.__jogador)
