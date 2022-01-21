bbb5a66095c23755e86e84c8919f3dde419da7dc
Alisson Rafael Barbosa <alissonrafaelbarbosa@gmail.com>
2022-01-20 21:08:45 -0300

    atualizando classes jogo e jogador

--- prototipo/Jogador.py
+++ prototipo/Jogador.py
@@ -1,6 +1,17 @@
+import pygame
 from abstractions.AbstractPersonagem import AbstractPersonagem
 from StatusJogador import StatusJogador
-from stats import JogadorStats
+
+
+JogadorStats = {
+    'vida': 10,
+    'ataque': 3,
+    'defesa': 2,
+    'vel': 4,
+    'vel_ataque': 1,
+    'arma_dano': 3,
+    'arma_alcance': 3
+}
 
 
 class Jogador(AbstractPersonagem):
@@ -8,17 +19,51 @@ class Jogador(AbstractPersonagem):
         super().__init__(stats=JogadorStats, posicao=posicao, tamanho=tamanho)
         self.__nome = nome
         self.__status = StatusJogador(self)
+        self.__sprite_path = ""
 
     @property
     def status(self):
         return self.__status
 
     @property
+    def sprite_path(self) -> str:
+        return self.__sprite_path
+
+    @property
     def nome(self):
         return self.__nome
 
-    def mover(self, key) -> tuple:
-        # Determinar qual a direção que o jogador escolheu
-        # Validar com o terreno se a nova posição é valida
-        # Se sim, atualizá-la
+    def lidar_inputs(self, keys) -> None:
+        self.__mover(keys)
+
+    def __mover(self, keys):
+        if keys[pygame.K_a]:
+            novo_x = self.hitbox.x - self.vel
+            nova_posicao = (novo_x, self.hitbox.y)
+            # Validar
+            if True:
+                self.hitbox.posicao = nova_posicao
+
+        if keys[pygame.K_d]:
+            novo_x = self.hitbox.x + self.vel
+            nova_posicao = (novo_x, self.hitbox.y)
+            if True:
+                self.hitbox.posicao = nova_posicao
+
+        if keys[pygame.K_w]:
+            novo_y = self.hitbox.y - self.vel
+            nova_posicao = (self.hitbox.x, novo_y)
+            if True:
+                self.hitbox.posicao = nova_posicao
+
+        if keys[pygame.K_s]:
+            novo_y = self.hitbox.y + self.vel
+            nova_posicao = (self.hitbox.x, novo_y)
+            if True:
+                self.hitbox.posicao = nova_posicao
+
+    def atacar():
+        pass
+
+    def mover():
         pass
--- prototipo/Jogo.py
+++ prototipo/Jogo.py
@@ -4,6 +4,7 @@ from Jogador import Jogador
 from Opcoes import Dificuldade, Opcoes
 from Comandos import Comandos
 from ControladorJogo import ControladorJogo
+from abstractions.AbstractFase import AbstractFase
 
 
 class Jogo:
@@ -13,23 +14,48 @@ class Jogo:
         self.__jogador = None
         self.__controlador = None
         self.__opcoes = None
+        self.__fase_atual = None
 
-    def play(self):
+        self.__FPS = 40
+
+    def start(self):
         self.__tela.mostrar_fundo()
         self.__carregar_dados()
 
+        clock = pygame.time.Clock()
         loop = True
         while loop:
-
-            for events in pygame.event.get():
-                if events.type == pygame.QUIT:
+            clock.tick(self.__FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     loop = False
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_RETURN:  # RETURN é o ENTER, dá inicio ao jogo no terreno 1
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:  # RETURN é o ENTER, dá inicio ao jogo no terreno 1
                         nova_fase = self.__controlador.proxima_fase()
-                        nova_fase.start(self.__tela)
+                        self.__rodar_fase(nova_fase)
+                        loop = False
 
                 pygame.display.update()
+
+    def __rodar_fase(self, fase: AbstractFase) -> None:
+        self.__fase_atual = fase
+        fase.start(self.__tela)
+        pygame.display.update()
+
+        clock = pygame.time.Clock()
+        main_loop = True
+        while main_loop:
+            clock.tick(self.__FPS)
+            keys_pressed = pygame.key.get_pressed()
+            self.__jogador.lidar_inputs(keys_pressed)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    main_loop = False
+
+            self.__fase_atual.ciclo(self.__tela)
+            pygame.display.update()
 
     def executar_comando(self):
         pass
@@ -38,7 +64,7 @@ class Jogo:
         pass
 
     def __escolher_dificuldade(self) -> Dificuldade:
-        return Dificuldade.medio
+        return Dificuldade.dificil
 
     def __escolher_nome(self) -> str:
         return 'Tatakae'
@@ -48,15 +74,6 @@ class Jogo:
         self.__opcoes = Opcoes(dificuldade)
 
         nome = self.__escolher_nome()
-        self.__jogador = Jogador((0, 0), (10, 10), nome)
+        self.__jogador = Jogador((0, 0), (50, 50), nome)
 
         self.__controlador = ControladorJogo(self.__jogador, self.__tela.tamanho, self.__opcoes)
-
-
-pygame.init()
-
-
-jogo = Jogo()
-jogo.play()
-
-pygame.quit()
