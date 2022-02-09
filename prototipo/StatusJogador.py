ebb5ef5bd70c0aa01075b3f40dc170258951e1d0
Paulo Eller Behr <pauloebehr@gmail.com>
2022-02-09 20:09:24 -0300

    Adição do status vida

--- prototipo/StatusJogador.py
+++ prototipo/StatusJogador.py
@@ -1,6 +1,5 @@
 from abstractions.AbstractPersonagem import AbstractPersonagem
-
-
+import pygame
 class StatusJogador():
     """Formata as informações do jogador para aparecer na tela"""
 
@@ -9,8 +8,21 @@ class StatusJogador():
 
     def vida():
         """Irá lidar com o retorno dos sprites para representar a vida"""
-        pass
+        font = pygame.font.Font(None, 40)
+        distancia_imagem = font.render(f'Vida:{Jogador.JogadorStats}', False, (142,0,28))
+        distancia_rect = distancia_imagem.get_rect(center = (400, 100))
+        return pygame.screen.blit(distancia_imagem, distancia_rect)
 
     def arma():
         """Irá lidar com o retorno dos sprites para representar a arma"""
         pass
+    
+    
+    
+    
+    def tela_de_distancia():
+        statusvida = int(pygame.time.get_ticks()/2000) - 0
+        distancia_imagem = pygame.font.render(f'Distância:{statusvida}', False, (142,0,28))
+        distancia_rect = distancia_imagem.get_rect(center = (400, 100))
+        pygame.screen.blit(distancia_imagem, distancia_rect)
+        return statusvida
