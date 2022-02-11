e059dfc85e0f19230f0305f462c27b55966b2452
Paulo Eller Behr <pauloebehr@gmail.com>
2022-02-10 22:23:44 -0300

    Correção do status jogador

--- prototipo/StatusJogador.py
+++ prototipo/StatusJogador.py
@@ -1,28 +1,18 @@
 from abstractions.AbstractPersonagem import AbstractPersonagem
 import pygame
-class StatusJogador():
-    """Formata as informações do jogador para aparecer na tela"""
 
+class StatusJogador():
+
+    """Formata as informações do jogador para aparecer na tela"""
     def __init__(self, jogador: AbstractPersonagem) -> None:
         self.__jogador = jogador
 
-    def vida():
+    def vida(self):
         """Irá lidar com o retorno dos sprites para representar a vida"""
-        font = pygame.font.Font(None, 40)
-        distancia_imagem = font.render(f'Vida:{Jogador.JogadorStats}', False, (142,0,28))
-        distancia_rect = distancia_imagem.get_rect(center = (400, 100))
-        return pygame.screen.blit(distancia_imagem, distancia_rect)
+        fonte = pygame.font.SysFont('arial', 40, False, False)
+        texto_formatado = fonte.render(f'Vida: {self.Jogador.vida}', True, (255,255,255))
+        return texto_formatado
 
     def arma():
         """Irá lidar com o retorno dos sprites para representar a arma"""
         pass
-    
-    
-    
-    
-    def tela_de_distancia():
-        statusvida = int(pygame.time.get_ticks()/2000) - 0
-        distancia_imagem = pygame.font.render(f'Distância:{statusvida}', False, (142,0,28))
-        distancia_rect = distancia_imagem.get_rect(center = (400, 100))
-        pygame.screen.blit(distancia_imagem, distancia_rect)
-        return statusvida
